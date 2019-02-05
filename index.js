cae2fe59a1179ed66d85541354d66a94dbfc33d2
Jordan Rivera <jordan.rivera34@gmail.com>
2019-02-05 17:04:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(`${name}`);
-  return`Welcome, ${name}. You are number ${katzDeliLine[i]} in line.`;
+  return`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
 
 // function nowServing(katzDeliLine) {
